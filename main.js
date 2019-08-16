83a9251d2b61edf88d6596ef10739029024a8bf8
Daniel Orio <danielislas3@live.com>
2019-08-16 10:15:50 -0500

    crud vue y localStorage

--- main.js
+++ main.js
@@ -1,35 +1,69 @@
-const app = new Vue({
-  el: '#app',
-  data:{
-    titulo:"Hola mundo con vue",
-    frutas:[
-      {nombre:'Pera', cantidad:10},
-      {nombre:'Manzana', cantidad:1},
-      {nombre:'Uva', cantidad:0},
+// const app = new Vue({
+//   el: '#app',
+//   data:{
+//     titulo:"Hola mundo con vue",
+//     frutas:[
+//       {nombre:'Pera', cantidad:10},
+//       {nombre:'Manzana', cantidad:1},
+//       {nombre:'Uva', cantidad:0},
 
-    ],
-    nuevaFruta:'',
-    cantidad:0,
-    total:0
+//     ],
+//     nuevaFruta:'',
+//     cantidad:0,
+//     total:0
+//   },
+//   methods:{
+//     agregarFruta(){
+//      this.frutas.push({
+//        nombre:this.nuevaFruta, cantidad: this.cantidad
+//      })
+//      this.nuevaFruta='',
+//      this.cantidad=0
+      
+//     }
+//   },
+//   computed:{
+//     sumarFrutas(){
+//       this.total=0
+//       for(fruta of this.frutas){
+//         this.total=this.total+fruta.cantidad
+//       }
+//       return this.total
+//     }
+
+//   }
+// })
+const app = new Vue({
+  el:'#app',
+  data:{
+    titulo:'Titulo de Vue',
+    tareas:[],
+    nuevaTarea:'',
   },
   methods:{
-    agregarFruta(){
-     this.frutas.push({
-       nombre:this.nuevaFruta, cantidad: this.cantidad
-     })
-     this.nuevaFruta='',
-     this.cantidad=0
-      
+    agregarTarea(){
+      this.tareas.push({
+        nombre:this.nuevaTarea,
+        estado:false
+      })
+      this.nuevaTarea=''
+      this.guardarLocal()
+    },
+    cambiarEstado(index){
+      this.tareas[index].estado=true
+      this.guardarLocal()
+    },
+    eliminarTarea(index){
+      this.tareas.splice(index,1 )
+     this.guardarLocal()
+    },
+    guardarLocal(){
+      localStorage.setItem('datosLocal',JSON.stringify(this.tareas))
     }
   },
-  computed:{
-    sumarFrutas(){
-      this.total=0
-      for(fruta of this.frutas){
-        this.total=this.total+fruta.cantidad
-      }
-      return this.total
-    }
-
+  created: function(){
+    const datosDB = JSON.parse(localStorage.getItem('datosLocal'))
+     
+    datosDB===null?this.tareas=[]:this.tareas=datosDB
   }
 })
